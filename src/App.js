6d12313214b6b8204c9d4ab7f099c06a5bb3ce0d
btguin <btguin@gmail.com>
2024-03-16 22:54:23 -0700

    Login updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { useAuth0 } from '@auth0/auth0-react';
 
-function App() {
+const App = () => {
+  const { loginWithRedirect, logout, user, isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) return <div>Loading...</div>;
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {!isAuthenticated ? (
+          <button onClick={() => loginWithRedirect()}>Log In</button>
+        ) : (
+          <>
+            <button onClick={() => logout({ returnTo: window.location.origin })}>
+              Log Out
+            </button>
+            <div>
+              <img src={user.picture} alt={user.name} />
+              <h2>{user.name}</h2>
+              <p>Email: {user.email}</p>
+            </div>
+          </>
+        )}
       </header>
     </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+
+const domain = "dev-ftfyfyyuswni2v1k.us.auth0.com";
+const clientId = "3AWz5kimk0CantIVfUbNHZS9BZaSzob8";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      redirectUri={window.location.origin}
+    >
+          <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
